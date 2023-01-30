c0b844dee1079985c30cc99d9d4e708a20a2278c
ravind0303 <ravind.pcf@gamil.com>
2023-01-30 16:40:00 +0530

    Java Class

---
+++ src/variable/Demo.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Demo {
+    public static void main(String[] args) {
+        String cityname="Agra";
+
+        int age=21;
+
+        double salery=2346.00;
+        System.out.println("city name  "  +cityname);
+        System.out.println("age  "   +age);
+        System.out.println("Salary  "  +salery);
+    }
+}
---
+++ src/variable/DemoVariable.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class DemoVariable {
+    public static void main(String[] args) {
+        String Mobilename="redmi";
+
+        int Ram= 64;
+
+        double Price=10500.32d;
+        System.out.println("Mobilename" +Mobilename);
+        System.out.println("Ram" + Ram);
+        System.out.println("Price" +Price);
+    }
+}
